629c78194986129f7f95ffb5a9ed587bebcda714
Rémi <lacrym095@gmail.com>
2016-11-16 16:39:04 +0100

    first commit

---
+++ GardeMnager/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GardeMnager
+//
+//  Created by Grunt on 16/11/2016.
+//  Copyright © 2016 Grunt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Garde Manger";
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
