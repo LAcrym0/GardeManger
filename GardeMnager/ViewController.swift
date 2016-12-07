ccaf613821752e22654671e2507f4213197dfb94
Rémi <lacrym095@gmail.com>
2016-12-07 11:48:20 +0100

    Last commit

--- GardeMnager/ViewController.swift
+++ GardeMnager/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import SDWebImage
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -48,30 +49,30 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             if findRecipeIndex(name: "Lasagnes") == -1 {
                 let lasagnes = Recipe(context: context)
                 lasagnes.name = "Lasagnes"
-                addIngredient(withName: "Oeufs", quantity: 2, toRecipe: lasagnes)
-                addIngredient(withName: "Tomates", quantity: 4, toRecipe: lasagnes)
-                addIngredient(withName: "Steaks", quantity: 5, toRecipe: lasagnes)
+                addIngredient(withName: "Oeufs", quantity: 2, url: "http://static.commentcamarche.net/sante-medecine.commentcamarche.net/faq/images/QqKAL9bu-oeufs.png",toRecipe: lasagnes)
+                addIngredient(withName: "Tomates", quantity: 4, url: "https://c5.staticflickr.com/4/3043/2694720316_65101af7a1_o.jpg", toRecipe: lasagnes)
+                addIngredient(withName: "Steaks", quantity: 5, url: "http://www.dangersalimentaires.com/wp-content/uploads/2012/10/steak-hache-congele.jpg", toRecipe: lasagnes)
                 recipes.append(lasagnes)
             }
             
             if findRecipeIndex(name: "Burger") == -1 {
                 let burger = Recipe(context: context)
                 burger.name = "Burger"
-                addIngredient(withName: "Bacon", quantity: 2, toRecipe: burger)
-                addIngredient(withName: "Pains", quantity: 2, toRecipe: burger)
-                addIngredient(withName: "Steaks", quantity: 1, toRecipe: burger)
-                addIngredient(withName: "Fromages", quantity: 2, toRecipe: burger)
-                addIngredient(withName: "Tomates", quantity: 1, toRecipe: burger)
+                addIngredient(withName: "Bacon", quantity: 2, url: "http://barbuzz.net/images/bacon-bacon.jpg", toRecipe: burger)
+                addIngredient(withName: "Pains", quantity: 2, url: "https://www.je-papote.com/wp-content/uploads/2014/02/pain-burger-maison.jpg", toRecipe: burger)
+                addIngredient(withName: "Steaks", quantity: 1, url: "http://www.dangersalimentaires.com/wp-content/uploads/2012/10/steak-hache-congele.jpg", toRecipe: burger)
+                addIngredient(withName: "Fromages", quantity: 2, url: "http://les-alpages.fr/wp-content/uploads/2014/09/cheddar-3.jpg", toRecipe: burger)
+                addIngredient(withName: "Tomates", quantity: 1, url: "https://c5.staticflickr.com/4/3043/2694720316_65101af7a1_o.jpg", toRecipe: burger)
                 recipes.append(burger)
             }
             
             if findRecipeIndex(name: "Tarte aux pommes") == -1 {
                 let applePie = Recipe(context: context)
                 applePie.name = "Tarte aux pommes"
-                addIngredient(withName: "Sucre", quantity: 3, toRecipe: applePie)
-                addIngredient(withName: "Pommes", quantity: 5, toRecipe: applePie)
-                addIngredient(withName: "Pâte feuilletée", quantity: 1, toRecipe: applePie)
-                addIngredient(withName: "Beurre", quantity: 2, toRecipe: applePie)
+                addIngredient(withName: "Sucre", quantity: 3, url: "http://media.rtl.fr/cache/VPaz3xCJinpFDHoQWoh51g/795v530-0/online/image/2015/0518/7778397968_sucre.jpg", toRecipe: applePie)
+                addIngredient(withName: "Pommes", quantity: 5, url: "http://www.aromemarket.com/549/10-ml-arome-pomme-fa-stark-flavor-apple.jpg", toRecipe: applePie)
+                addIngredient(withName: "Pâte feuilletée", quantity: 1, url: "http://img.cac.pmdstatic.net/fit/http.3A.2F.2Fwww.2Ecuisineactuelle.2Efr.2Fvar.2Fcui.2Fstorage.2Fimages.2Fvideos.2F1-technique-1-minute.2Fcomment-reussir-sa-pate-feuilletee.2F1847811-1-fre-FR.2Fcomment-reussir-sa-pate-feuilletee.2Ejpg/734x367/crop-from/center/comment-reussir-sa-pate-feuilletee.jpeg", toRecipe: applePie)
+                addIngredient(withName: "Beurre", quantity: 2, url: "http://www.aliments.com/images/nb/beurre.jpg", toRecipe: applePie)
                 recipes.append(applePie)
             }
             
@@ -96,8 +97,14 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         navigationItem.rightBarButtonItems = [edit, add]
         
         self.title = "Garde Manger"
+        Timer.scheduledTimer(timeInterval: 4, target: self, selector: #selector(self.update), userInfo: nil, repeats: false);
         
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func update() {
+        //to display images async
+        self.tableView.reloadData()
     }
     
     override func didReceiveMemoryWarning() {
@@ -121,10 +128,16 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             quantityTextField.keyboardType = UIKeyboardType.numberPad
         }
         
+        alert.addTextField { (imageUrlTextField) in
+            imageUrlTextField.text = ""
+            imageUrlTextField.placeholder = "URL de l'image"
+            imageUrlTextField.keyboardType = UIKeyboardType.URL
+        }
         
+        alert.addAction(UIAlertAction(title: "Annuler", style: .destructive, handler: nil))
         
         // Grab the value from the text field, and print it when the user clicks OK.
-        alert.addAction(UIAlertAction(title: "Ajouter l'image", style: .default, handler: { (_) in
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
             if alert.textFields![0].text?.characters.count != 0 {
                 self.ingredientName = alert.textFields![0].text // Force unwrapping because we know it exists.
             } else {
@@ -136,23 +149,21 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
             } else {
                 self.ingredientQuantity = Int32(alert.textFields![1].text!)
             }
-            // let imageUrlField = alert.textFields![2]
-            //print("Text field: \(textField.text)")
+            self.ingredientImageUrl = alert.textFields![2].text?.characters.count == 0 ? "" : alert.textFields![2].text
+            self.createNewIngredient(name: self.ingredientName!, url: self.ingredientImageUrl!, quantity: self.ingredientQuantity!)
+            self.tableView.reloadData()
             
-            self.showImagePicker()
         }))
         
-        alert.addAction(UIAlertAction(title: "Annuler", style: .destructive, handler: nil))
-        
-        // 4. Present the alert.
+        // Present the alert.
         self.present(alert, animated: true, completion: nil)
     }
     
-    func addIngredient(withName: String, quantity: Int, toRecipe: Recipe) {
+    func addIngredient(withName: String, quantity: Int, url: String, toRecipe: Recipe) {
         let index = findIngredientIndex(name: withName)
         var id: String
         if index == -1 {
-            id = createNewIngredient(name: withName)
+            id = createNewIngredient(name: withName, url: url)
         } else {
             id = ingredients[index].objectID.uriRepresentation().absoluteString
         }
@@ -167,64 +178,19 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         }
     }
     
-    func createNewIngredient(name: String) -> String {
+    func createNewIngredient(name: String, url: String) -> String {
+        return createNewIngredient(name: name, url: url, quantity: 50)
+    }
+    
+    func createNewIngredient(name: String, url: String, quantity: Int32) -> String {
         let ingredient = Ingredient(context: self.context!)
-        ingredient.quantity = 50
+        ingredient.quantity = quantity
         ingredient.name = name
+        ingredient.imageUrl = url
         try? self.context?.save()
         let id = ingredient.objectID.uriRepresentation().absoluteString
         ingredients.append(ingredient)
         return id
-    }
-    
-    func showImagePicker() {
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-            //we set the UIImagePickerController and its paremeters
-            let pickerController = UIImagePickerController()
-            pickerController.sourceType = .photoLibrary //source = galery
-            pickerController.allowsEditing = false //after choosing the picture the user can't edit it
-            pickerController.delegate = self
-            pickerController.modalTransitionStyle = .crossDissolve
-            
-            //we show the pickerController
-            self.present(pickerController, animated: true, completion: nil)
-            
-        }
-    }
-    
-    func save(url: String) {
-        print (url)
-        if (self.context != nil) {
-            let index = findIngredientIndex(name: ingredientName!)
-            if index == -1 {
-                let ingredient = Ingredient(context: self.context!)
-                ingredient.name = ingredientName
-                ingredient.imageUrl = url
-                ingredient.quantity = ingredientQuantity!
-                ingredients.append(ingredient)
-            } else {
-                ingredients[index].quantity += Int32(ingredientQuantity!)
-            }
-            self.tableView.reloadData()
-            
-            do { try! self.context?.save() }
-            
-            ingredientName = ""
-            ingredientQuantity = 0
-            ingredientImageUrl = ""
-            
-        }
-    }
-    
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        let imageURL = info[UIImagePickerControllerReferenceURL] as! NSURL
-        let imageName = imageURL.lastPathComponent
-        let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first! as String
-        let localPath = documentDirectory + imageName!
-        let photoURL = NSURL(fileURLWithPath: localPath)
-        picker.dismiss(animated: true, completion: nil)
-        save(url: (photoURL.path)!)
     }
     
     func saveImageDocumentDirectory(ingredientName: String){
@@ -390,28 +356,19 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         let cellIdentifier = "ElementCell"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
             ?? UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
-        
+        cell.imageView?.image = UIImage(named: "placeholder.png")
         // Adding the right informations
         
         if ingredient.imageUrl != nil {
-            if FileManager.default.fileExists(atPath: ingredient.imageUrl!) {
+            /*if FileManager.default.fileExists(atPath: ingredient.imageUrl!) {
                 print("File exists")
             } else {
                 print("File doesn't exist")
-            }
+            }*/
             let url = URL.init(string: ingredient.imageUrl!)
             if ingredient.imageUrl?.characters.count != 0 && url != nil {
-                if let data = try? Data.init(contentsOf: url!) {
-                    cell.imageView?.image = UIImage(data: data as Data)
-                }
-            } else if let url = URL.init(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Question_mark_alternate.svg/langfr-90px-Question_mark_alternate.svg.png") {
-                if let data = try? Data.init(contentsOf: url) {
-                    cell.imageView?.image = UIImage(data: data as Data)
-                }
-            }
-        } else if let url = URL.init(string: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Question_mark_alternate.svg/langfr-90px-Question_mark_alternate.svg.png") {
-            if let data = try? Data.init(contentsOf: url) {
-                cell.imageView?.image = UIImage(data: data as Data)
+                //downloadImage(url: URL(string: ingredient.imageUrl!)!, cell: cell)
+                cell.imageView?.sd_setImage(with: NSURL(string: ingredient.imageUrl!) as URL!)
             }
         }
         
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, ‘8.0’
+
+target 'GardeMnager' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'SDWebImage', '~>3.8'
+
+  # Pods for GardeMnager
+
+
+end
